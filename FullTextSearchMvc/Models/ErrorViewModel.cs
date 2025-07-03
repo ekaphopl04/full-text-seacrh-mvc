ab632c5a669dbc65a22a7b7d92d090abef0208e4
ekaphopl04 <ekaphop.l04@gmail.com>
2025-07-03 14:14:05 +0700

    feat: initialize MVC project with default templates and dependencies

---
+++ FullTextSearchMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FullTextSearchMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
